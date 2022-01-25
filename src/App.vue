7363a50c04a8e5cf3e026dcb11c2cfef033a87ac
Zpangxie <870074823@qq.com>
2022-01-25 18:58:42 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
 	<h1>一个人的信息</h1>
-	
+	<h2>姓名：{{name}}</h2>
+	<h2>年龄：{{age}}</h2>
+	<h2>性别：{{sex}}</h2>
+	<h2>a的值是：{{a}}</h2>
+	<button @click="sayHello">说话(Vue3所配置的——sayHello)</button>
+	<br>
+	<br>
+	<button @click="sayWelcome">说话(Vue2所配置的——sayWelcome)</button>
+	<br>
+	<br>
+	<button @click="test1">测试一下在Vue2的配置中去读取Vue3中的数据、方法</button>
+	<br>
+	<br>
+	<button @click="test2">测试一下在Vue3的setup配置中去读取Vue2中的数据、方法</button>
 
 </template>
 
@@ -26,7 +39,7 @@
 			}
 		},
 		//此处只是测试一下setup，暂时不考虑响应式的问题。
-		async setup(){
+		setup(){
 			//数据
 			let name = '张三'
 			let age = 18
