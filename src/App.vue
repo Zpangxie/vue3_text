e96f9942ab7993c0224f783727601b9cbd5dea9a
Zpangxie <870074823@qq.com>
2022-01-25 17:56:02 +0800

    vue3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>一个人的信息</h1>
+	
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	// import {h} from 'vue'
+	export default {
+		name: 'App',
+		data() {
+			return {
+				sex:'男',
+				a:100
+			}
+		},
+		methods: {
+			sayWelcome(){
+				alert('欢迎来到尚硅谷学习')
+			},
+			test1(){
+				console.log(this.sex)
+				console.log(this.name)
+				console.log(this.age)
+				console.log(this.sayHello)
+			}
+		},
+		//此处只是测试一下setup，暂时不考虑响应式的问题。
+		async setup(){
+			//数据
+			let name = '张三'
+			let age = 18
+			let a = 200
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+			//方法
+			function sayHello(){
+				alert(`我叫${name}，我${age}岁了，你好啊！`)
+			}
+			function test2(){
+				console.log(name)
+				console.log(age)
+				console.log(sayHello)
+				console.log(this.sex)
+				console.log(this.sayWelcome)
+			}
+
+			//返回一个对象（常用）
+			return {
+				name,
+				age,
+				sayHello,
+				test2,
+				a
+			}
+
+			//返回一个函数（渲染函数）
+			// return ()=> h('h1','尚硅谷')
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
